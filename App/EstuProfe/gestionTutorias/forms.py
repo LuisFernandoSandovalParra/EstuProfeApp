b79e094e7f71fa5946787402e15fc6b2015fce48
Luis Sandoval <52140762+LuisFernandoSandovalParra@users.noreply.github.com>
2022-07-26 16:53:43 -0500

    Delete App/EstuProfe directory

--- App/EstuProfe/gestionTutorias/forms.py
+++
@@ -1,7 +0,0 @@
-import email
-from django import forms
-
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField()
-    type = forms.ChoiceField(choices=['ESTUDIANTE', 'TUTOR'])
